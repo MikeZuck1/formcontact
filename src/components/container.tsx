fef299553e717f375f5b33612c42cd42270eb9cc
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-01-28 21:46:32 +0100

    add testInput file.

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,9 @@
-// import { Button } from "./ui/button"; 
+// import { Button } from "./ui/button";
+// import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 const container = () => {
-
+  // const [name, setName] = useState(); 
+  
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="p-5 w-[35rem] h-[35rem] rounded-xl bg-white shadow-lg">
@@ -8,32 +11,20 @@ const container = () => {
         <div className="flex flex-row gap-5 mt-5">
           {/* Input First Name */}
           <div className="flex flex-col">
-            <label htmlFor="" className="mb-2">
-              First Name <span className="text-[hsl(186,15%,59%)]">*</span>
+            <label htmlFor="" className="mb-1">
+              First Name <span className="text-greenMedium">*</span>
             </label>
-            <input
-              type="text"
-              name=""
-              id=""
-              className="w-[15rem] h-[2.2rem] rounded-sm border-[0.5px] border-[hsl(186,15%,59%)] text-black p-2"
-            />
+            <Input className="w-[15.5rem]" />
           </div>
-
           {/* Input Last Name */}
           <div className="flex flex-col">
-            <label htmlFor="" className="mb-2">
-              Last Name <span className="text-[hsl(186,15%,59%)]">*</span>
+            <label htmlFor="" className="mb-1">
+              Last Name <span className="text-greenMedium">*</span>
             </label>
-            <input
-              type="text"
-              name=""
-              id=""
-              className="w-[15rem] h-[2.2rem] rounded-sm border-[0.5px] border-[hsl(186,15%,59%)] text-black p-2"
-            />
+            <Input className="w-[15.5rem]" />
           </div>
         </div>
       </div>
-      {/* Input content */}
     </div>
   );
 };
